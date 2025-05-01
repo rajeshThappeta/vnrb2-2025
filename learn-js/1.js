5cf4ce1cc09483282941b473cc2bf8f2d0c1a582
rajesh <rajesh.t7982@gmail.com>
2025-05-01 20:43:59 +0530

    js-day-1

---
+++ learn-js/1.js
@@ -0,0 +1,36 @@
+//Create variables
+//In js, variables dont have datatypes,but values have
+//JS is dynamically typed programming language
+let a;
+
+//print datatype
+console.log("datatype of a is ", typeof a)
+console.log('value of a is ', a) //value of a is 
+
+a = 10;
+console.log("datatype of a is ", typeof a)
+console.log('value of a is ', a)
+
+a = 'hello'
+console.log("datatype of a is ", typeof a)
+console.log('value of a is ', a)
+
+a = true;
+console.log("datatype of a is ", typeof a)
+console.log('value of a is ', a)
+
+
+a = [10, 20, 30]
+console.log("datatype of a is ", typeof a)
+console.log('value of a is ', a)
+
+a = {
+    name: 'ravi',
+    age: 20
+}
+console.log("datatype of a is ", typeof a)
+console.log('value of a is ', a)
+
+
+
+let x=10;
---
+++ learn-js/2.js
@@ -0,0 +1,34 @@
+//Arithemtic operatots( + - * / % ++ --)
+let a = 10;
+let b = 50;
+console.log(a / b)
+console.log(a % b)
+
+//Assignment operator ( = )
+//Equal to ( ==)
+//Strict equal to ( === )
+console.log(a == b)
+
+let x = 11
+let y = '11'
+
+console.log(x == y)
+console.log(x === y)
+
+//logical operators
+//logical AND (&&)
+console.log(a > 5 && b < 7)
+//logical OR ( ||)
+console.log(a > 5 || b > 7)
+//logical NOT ( ! )
+
+
+//conditional operator( ternary operator ?:)
+//condition ? expression-1 : expression-2
+a>b ? console.log("a is big") : console.log("b is big")
+
+
+
+
+console.log("value of a is ",a ," and b is ",b)
+console.log(` value of a is ${a} and b is ${b}` )
